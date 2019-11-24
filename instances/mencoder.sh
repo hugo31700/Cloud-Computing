2c7f54b47b86fdf6786a25c26254f20e53cde6f9
Jordan Sandri <jsandri@enseirb-matmeca.fr>
2019-11-24 16:20:02 +0100

    ok

---
+++ instances/mencoder.sh
@@ -0,0 +1,1 @@
+ssh -i "../keys/ubuntu_sth_0.pem" ubuntu@ec2-13-53-48-187.eu-north-1.compute.amazonaws.com
---
+++ instances/webpage.sh
@@ -0,0 +1,1 @@
+ssh -i "../keys/web-server.pem" ubuntu@ec2-13-53-184-162.eu-north-1.compute.amazonaws.com
