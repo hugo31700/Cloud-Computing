4fda895c4d7da1c07dca5f4c78cbe92037de472f
vyhv00 <vojta.vyhnal@gmail.com>
2019-11-30 17:10:11 +0100

    Webpage cointains radiobutton for format. PHP prepared for save in S3 bucket and retrieve from S3 bucket.

--- webpage/process.php
+++ webpage/process.php
@@ -1,52 +1,39 @@
 <?php
-//save file
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-/*if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-} */
-
-// here will be placed the call of mencoder
-$filepath = $target_file;
-
-
-//send to downlaod
-if(file_exists($filepath)) {
-        echo "file exists";
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($filepath));
-        flush(); // Flush system output buffer
-        readfile($_FILES["fileToUpload"]["name"]);
-        exit;
-    }
-    else{
-        echo "file not found";
-    }
+//if(isset($_POST["submit"])) {
+//	if(isset($_FILES['fileToUpload'])){
+    //$path = $_FILES['fileToUpload']['name'];
+    //$ext = pathinfo($path, PATHINFO_EXTENSION);
+        
+		$file_name = $_FILES['fileToUpload']['name'];   
+		$temp_file_location = $_FILES['fileToUpload']['tmp_name']; 
+		require 'vendor/autoload.php';
+		$s3 = new Aws\S3\S3Client([
+			'region'  => 'eu-north-1',
+			'version' => 'latest',
+			'credentials' => [
+				'key'    => "AKIATZUDUIJ6GAXJM6MI",
+				'secret' => "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",
+			]
+		]);		
+		$result = $s3->putObject([
+			'Bucket' => 'storage-mencoder',
+			'Key'    => $file_name,
+			'SourceFile' => $temp_file_location			
+		]);
+		//var_dump($result);
+        
+        try {
+        // Get the object.
+        $answer = $s3->getObject([
+            'Bucket' => 'storage-mencoder',
+            'Key'    => $file_name
+        ]);
+        // Display the object in the browser.
+        echo "worked";
+        header("Content-Type: {$answer['ContentType']}");
+        echo $answer['Body'];
+        } catch (S3Exception $e) {
+            echo $e->getMessage() . PHP_EOL;
+        }   
+//	}
 ?>
-
-
