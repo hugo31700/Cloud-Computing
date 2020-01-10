8812fe6a446189e7e173d263982a5550889da202
hugo <mhug31@gmail.com>
2020-01-10 06:31:29 -0800

    men

--- instances/mencoder.sh
+++ instances/mencoder.sh
@@ -1,1 +1,1 @@
-ssh -i "../keys/ubuntu_sth_0.pem" ubuntu@ec2-13-53-48-187.eu-north-1.compute.amazonaws.com
+ssh -i "../keys/ubuntu_sth_0.pem" ubuntu@ec2-13-53-46-213.eu-north-1.compute.amazonaws.com
